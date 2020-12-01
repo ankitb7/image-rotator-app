270b97cc9948b794881dee2bc315547f19aa6b2f
Ankit Borawake <ankit.borawake@amdocs.com>
2020-12-01 16:09:36 +0200

    Refactored code

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders application header', () => {
   render(<App />);
   const header = screen.getByRole('banner');
   expect(header).toBeInTheDocument();
